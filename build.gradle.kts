a0ce74031528e45b2211c579eae0ab6c4111b33c
Johannes <johannesgt44@gmail.com>
2026-04-07 14:43:36 +0200

    jaja

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("application")
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.acme"
@@ -9,10 +11,41 @@ repositories {
     mavenCentral()
 }
 
+val grpcVersion = "1.65.1"
+val protobufVersion = "3.25.5"
+
 dependencies {
+    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    compileOnly("org.apache.tomcat:annotations-api:6.0.53")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+            }
+        }
+    }
+}
+
+application {
+    mainClass = "com.acme.rechnung.metadata.InvoiceMetadataServer"
 }
 
 tasks.test {
