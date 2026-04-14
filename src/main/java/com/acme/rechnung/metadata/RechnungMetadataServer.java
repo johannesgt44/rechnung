c8ec9ce15b8b2291abaf822e2aeeac814d20a85d
Nick Magin <mani1040@h-ka.de>
2026-04-14 10:53:57 +0200

    Kommentare ergänzt

--- src/main/java/com/acme/rechnung/metadata/RechnungMetadataServer.java
+++ src/main/java/com/acme/rechnung/metadata/RechnungMetadataServer.java
@@ -7,22 +7,36 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import java.io.IOException;
 
+/**
+ * Haupteinstiegspunkt
+ */
 public final class RechnungMetadataServer {
+
+    //Standart-Port wird gesetzt
     private static final int DEFAULT_PORT = 50051;
 
     private final Server server;
 
+    //Konstruktor um den Server zusammenzubauen inkl. Abhängigkeiten
     private RechnungMetadataServer(int port) {
+        //Repository als "Datenlager" erstellen
         RechnungRepository repository = new RechnungRepository();
+        //Service zum Lesen von Rechnungen wird initialisiert
         RechnungService rechnungService = new RechnungService(repository);
+        //Service zum Schreiben von Rechnungen wird initialisiert
         RechnungWriteService rechnungWriteService = new RechnungWriteService(repository);
 
+        //Server zusammenbauen
         this.server = ServerBuilder.forPort(port)
                 .addService(new RechnungMetadataGrpcService(rechnungService, rechnungWriteService))
                 .build();
     }
 
+    /**
+     *Startpunkt der Anwendung
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
+        //Prüfen, ob ein Port angegeben wurde. Ansonsten Standart-Port verwenden
         int port = args.length > 0 ? Integer.parseInt(args[0]) : DEFAULT_PORT;
         RechnungMetadataServer rechnungMetadataServer = new RechnungMetadataServer(port);
         rechnungMetadataServer.start();
